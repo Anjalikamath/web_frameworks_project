a69569f52dbd4eb3c8b7818395fdb8ca5b9dd30d
Dabb <kamathanjali1999@gmail.com>
2019-04-20 23:31:58 +0530

    Modified templates to display posts from database.

--- blo/views.py
+++ blo/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Books
 # Create your views here.
 def post_list(request):
-    return render(request,'blo/homepg.html',{})
+    posts=Books.objects.all()
+    return render(request,'blo/homepg.html',{'posts':posts})
