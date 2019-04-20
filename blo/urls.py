45e9df13faf71a0de9c7203ba9cd71603498d2b6
Dabb <kamathanjali1999@gmail.com>
2019-04-21 01:45:16 +0530

    Modified templates to display posts from database.

--- blo/urls.py
+++ blo/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
-
+from django.conf.urls import url
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('', views.homePg, name='homePg'),
+    url(r'^blo/contacts',views.contact,name='contact'),
+    url(r'^blo/books',views.books,name='books'),
+    url(r'^blo/homePg',views.homePg,name='homePg'),
+    url(r'^blo/Fiction',views.fiction,name='fiction'),
+    url(r'^blo/NonFiction',views.nonfiction,name='nonfiction'),
+    url(r'^blo/Education',views.education,name='education'),
 ]
--- blo/views.py
+++ blo/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from .models import Books
 # Create your views here.
-def post_list(request):
-    posts=Books.objects.all()
-    return render(request,'blo/homepg.html',{'posts':posts})
+def homePg(request):
+    #posts=Books.objects.all()
+    return render(request,'blo/homepg.html',{})
+def contact(request):
+    return render(request,'blo/contacts.html',{})
+def books(request):
+    return render(request,'blo/books.html',{})
+#def homePage(request):
+    return render(request,'blo/homepg.html',{})
+def fiction(request):
+    return render(request,'blo/Fiction.html',{})
+def nonfiction(request):
+    return render(request,'blo/NonFiction.html',{})
+def education(request):
+    return render(request,'blo/Education.html',{})
